ce60b741d278578c3fd491425bd73b2da1fbad9f
whwebdev <adam.willis@woodhouse.com>
2014-04-07 00:53:26 -0500

    adding rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VirtualAtrributes::Application.config.secret_key_base = '2efddbbb9e76a73f7fcae385a0f4be0fd79c687c48a36c1288d8655db63c1ad21ea807bd73ece929e5aeb29549fcc11fbbe0f96046d9c75a11e7da44d7c91fcd'
